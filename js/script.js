f8a67c46137851241fef8b33931c64d353ec98a3
BinariIntelligence <157888261+BinariIntelligence@users.noreply.github.com>
2024-02-02 16:55:38 +0530

    Adding about and Contact Us

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ window.addEventListener("load", function () {
 
 
 
-// MOBILE NAV TOGGLE
+ // PAGE NAV TOGGLE
 
 const [navTogglers, navLinks, navbar, overlay] = [
   document.querySelectorAll("[data-nav-toggler]"),
@@ -48,9 +48,34 @@ const closeNav = function () {
 
 addEventOnElements(navLinks, "click", closeNav);
 
+// CONTACT NAV TOGGLE
+
+const [cnavTogglers, cnavLinks, cnavbar, coverlay] = [
+  document.querySelectorAll("[data-cnav-toggler]"),
+  document.querySelectorAll("[data-cnav-link]"),
+  document.querySelector("[data-cnavbar]"),
+  document.querySelector("[data-coverlay]")
+];
+
+const togglecNav = function () {
+  cnavbar.classList.toggle("active");
+  coverlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElements(cnavTogglers, "click", togglecNav);
+
+const closecNav = function () {
+  cnavbar.classList.remove("active");
+  coverlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElements(cnavLinks, "click", closecNav);
 
 
-// HEADER
+
+// HEADER & FOOTER
 
 const header = document.querySelector("[data-header]");
 
@@ -64,11 +89,15 @@ const activeElementOnScroll = function () {
 
 window.addEventListener("scroll", activeElementOnScroll);
 
+/**
+ * TEXT ANIMATION EFFECT FOR HERO SECTION
+ */
+
 const wordBoxes = document.querySelectorAll("[word-effect]");
 
 let activeWordBoxIndex  = 0;
 let lastActiveWordBoxIndex = 0;
-let totalWordBoxDelay = 3;
+let totalWordBoxDelay = 2;
 
 const setWordEffect = function () {
   // loop through all letter boxes
@@ -100,115 +129,6 @@ window.addEventListener("load", setWordEffect);
 
 
 /**
- * TEXT ANIMATION EFFECT FOR HERO SECTION
- */
-
-// const letterBoxes = document.querySelectorAll("[data-letter-effect]");
-
-// let activeLetterBoxIndex = 0;
-// let lastActiveLetterBoxIndex = 0;
-// let totalLetterBoxDelay = 0;
-
-// const setLetterEffect = function () {
-
-//   // loop through all letter boxes
-//   for (let i = 0; i < letterBoxes.length; i++) {
-//     // set initial animation delay
-//     let letterAnimationDelay = 0;
-
-//     // get all character from the current letter box
-//     const letters = letterBoxes[i].textContent.trim();
-//     // remove all character from the current letter box
-//     letterBoxes[i].textContent = "";
-
-//     // loop through all letters
-//     for (let j = 0; j < letters.length; j++) {
-
-//       // create a span
-//       const span = document.createElement("span");
-
-//       // set animation delay on span
-//       span.style.animationDelay = `${letterAnimationDelay}s`;
-
-//       // set the "in" class on the span, if current letter box is active
-//       // otherwise class is "out"
-//       if (i === activeLetterBoxIndex) {
-//         span.classList.add("in");
-//       } else {
-//         span.classList.add("out");
-//       }
-
-//       // pass current letter into span
-//       span.textContent = letters[j];
-
-//       // add space class on span, when current letter contain space
-//       if (letters[j] === " ") span.classList.add("space");
-
-//       // pass the span on current letter box
-//       letterBoxes[i].appendChild(span);
-
-//       // skip letterAnimationDelay when loop is in the last index
-//       if (j >= letters.length - 1) break;
-//       // otherwise update
-//       letterAnimationDelay += 0.05;
-
-//     }
-
-//     // get total delay of active letter box
-//     if (i === activeLetterBoxIndex) {
-//       totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
-//     }
-
-//     // add active class on last active letter box
-//     if (i === lastActiveLetterBoxIndex) {
-//       letterBoxes[i].classList.add("active");
-//     } else {
-//       letterBoxes[i].classList.remove("active");
-//     }
-
-//   }
-
-//   setTimeout(function () {
-//     lastActiveLetterBoxIndex = activeLetterBoxIndex;
-
-//     // update activeLetterBoxIndex based on total letter boxes
-//     activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex = 0 : activeLetterBoxIndex++;
-
-//     setLetterEffect();
-//   }, (totalLetterBoxDelay * 1000) + 3000);
-
-// }
-
-// // call the letter effect function after window loaded
-// window.addEventListener("load", setLetterEffect);
-
-
-
-/**
- * BACK TO TOP BUTTON
- */
-
-// const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-// window.addEventListener("scroll", function () {
-//   const bodyHeight = document.body.scrollHeight;
-//   const windowHeight = window.innerHeight;
-//   const scrollEndPos = bodyHeight - windowHeight;
-//   const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
-
-//   backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
-
-//   // visible back top btn when scrolled 5% of the page
-//   if (totalScrollPercent > 5) {
-//     backTopBtn.classList.add("show");
-//   } else {
-//     backTopBtn.classList.remove("show");
-//   }
-// });
-
-
-
-/**
  * SCROLL REVEAL
  */
 
@@ -229,43 +149,3 @@ const scrollReveal = function () {
 window.addEventListener("scroll", scrollReveal);
 
 scrollReveal();
-
-
-
-/**
- * CUSTOM CURSOR
- */
-
-// const cursor = document.querySelector("[data-cursor]");
-// const anchorElements = document.querySelectorAll("a");
-// const buttons = document.querySelectorAll("button");
-
-// change cursorElement position based on cursor move
-// document.body.addEventListener("mousemove", function (event) {
-//   setTimeout(function () {
-//     cursor.style.top = `${event.clientY}px`;
-//     cursor.style.left = `${event.clientX}px`;
-//   }, 100);
-// });
-
-// add cursor hoverd class
-// const hoverActive = function () { cursor.classList.add("hovered"); }
-
-// remove cursor hovered class
-// const hoverDeactive = function () { cursor.classList.remove("hovered"); }
-
-// add hover effect on cursor, when hover on any button or hyperlink
-// addEventOnElements(anchorElements, "mouseover", hoverActive);
-// addEventOnElements(anchorElements, "mouseout", hoverDeactive);
-// addEventOnElements(buttons, "mouseover", hoverActive);
-// addEventOnElements(buttons, "mouseout", hoverDeactive);
-
-// add disabled class on cursorElement, when mouse out of body
-// document.body.addEventListener("mouseout", function () {
-//   cursor.classList.add("disabled");
-// });
-
-// remove diabled class on cursorElement, when mouse in the body
-// document.body.addEventListener("mouseover", function () {
-//   cursor.classList.remove("disabled");
-// });
