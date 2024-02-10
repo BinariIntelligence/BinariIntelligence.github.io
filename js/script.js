a5e3c0f64daf518a550b887896cf84b5725f4f00
BinariIntelligence <157888261+BinariIntelligence@users.noreply.github.com>
2024-02-10 12:41:55 +0530

    refining about and style

--- js/script.js
+++ js/script.js
@@ -77,17 +77,17 @@ addEventOnElements(cnavLinks, "click", closecNav);
 
 // HEADER & FOOTER
 
-const header = document.querySelector("[data-header]");
+// const header = document.querySelector("[data-header]");
 
-const activeElementOnScroll = function () {
-  if (window.scrollY > 50) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-}
+// const activeElementOnScroll = function () {
+//   if (window.scrollY > 50) {
+//     header.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//   }
+// }
 
-window.addEventListener("scroll", activeElementOnScroll);
+// window.addEventListener("scroll", activeElementOnScroll);
 
 /**
  * TEXT ANIMATION EFFECT FOR HERO SECTION
